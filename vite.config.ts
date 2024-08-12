fab0fb23c4039fb6412db7131f385b7140fe3d14
nadyayusova <60035655+nadyayusova@users.noreply.github.com>
2024-08-12 15:43:58 +0300

    fix: fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://nadyayusova.github.io/test-task-labint',
   plugins: [react()],
 });
