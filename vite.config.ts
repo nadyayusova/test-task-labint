3774316c74fee56b30e8639910f2997bc4a57943
nadyayusova <60035655+nadyayusova@users.noreply.github.com>
2024-08-12 15:07:47 +0300

    fix: fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://nadyayusova.github.io/test-task-labint',
   plugins: [react()],
-})
+});
